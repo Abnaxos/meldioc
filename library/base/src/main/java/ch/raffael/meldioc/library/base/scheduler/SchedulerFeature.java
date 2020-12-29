be506aaddd24a32cf958dc2b74bf580626aaf7cd
Raffael Herzog <herzog@raffael.ch>
2020-12-29 09:03:52 +0100

    Apply 'shared' -> 'singleton' rename to scheduler

--- library/base/src/main/java/ch/raffael/meldioc/library/base/scheduler/SchedulerFeature.java
+++ library/base/src/main/java/ch/raffael/meldioc/library/base/scheduler/SchedulerFeature.java
@@ -1,3 +1,25 @@
+/*
+ *  Copyright (c) 2020 Raffael Herzog
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
+
 package ch.raffael.meldioc.library.base.scheduler;
 
 import ch.raffael.meldioc.Feature;
@@ -20,7 +42,7 @@ public interface SchedulerFeature {
   @Parameter.Prefix("scheduler")
   abstract class Default implements SchedulerFeature {
 
-    @Provision(shared = true)
+    @Provision(singleton = true)
     @Override
     public Scheduler scheduler() {
       return DefaultScheduler.withExecutor(executor())
@@ -54,7 +76,7 @@ public interface SchedulerFeature {
 
   @Feature
   abstract class WithShutdown extends Default implements ShutdownFeature {
-    @Provision(shared = true)
+    @Provision(singleton = true)
     @Override
     public Scheduler scheduler() {
       var scheduler = (DefaultScheduler) super.scheduler();
